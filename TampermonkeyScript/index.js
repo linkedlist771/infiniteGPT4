57a8aa12c8bb0406bba4864f8193354bc0030e34
LiDing <72634327+linkedlist771@users.noreply.github.com>
2023-05-27 17:24:00 +0800

    Feature：进行油猴脚本的更新
    添加切换按钮以启用/禁用脚本
    
    本次提交添加了一个切换按钮，允许用户启用或禁用脚本。该按钮位于页面顶部，可用于打开或关闭脚本。按钮的状态保存在本地存储中，因此它将在页面重新加载时保持不变。
    
    本次提交还包括一个警告消息，第一次启用脚本时会显示该消息。该消息警告用户使用脚本后可能会导致账户被封禁并退款，建议用户谨慎使用。

--- TampermonkeyScript/index.js
+++ TampermonkeyScript/index.js
@@ -1,25 +1,65 @@
 // ==UserScript==
 // @name        无限GPT4
 // @namespace   https://github.com/linkedlist771
-// @description 无限使用GPT4
+// @description 无限使用GPT4,有人在使用插件后，出现封号退款的情况，是不是由于插件导致的不清楚，酌情使用哦。
 // @license MIT
 // @include     *
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
+    // 添加启动/不启动的UI选项
+    let isScriptEnabled = localStorage.getItem('isScriptEnabled') ? true : false;
+ 
+    // 按钮点击事件
+    function toggleScript() {
+        isScriptEnabled = !isScriptEnabled;
+        localStorage.setItem('isScriptEnabled', isScriptEnabled);
+        toggleButton.textContent = isScriptEnabled ? '停止无限GPT-4插件' : '启动无限GPT-4插件';
+        if(isScriptEnabled && !localStorage.getItem('alertShown')){
+            alert("有人在使用插件后，出现封号退款的情况，是不是由于插件导致的不清楚，酌情使用哦。");
+            localStorage.setItem('alertShown', true);
+        }
+    }
+ 
+    let toggleButton = document.createElement('button');
+    toggleButton.style.position = 'fixed';
+    toggleButton.style.top = '10px';
+    toggleButton.style.left = '50%';
+    toggleButton.style.transform = 'translateX(-50%)';
+    toggleButton.style.backgroundColor = 'red';
+    toggleButton.style.color = 'white';
+    toggleButton.style.fontSize = '20px';
+    toggleButton.style.padding = '10px 20px';
+    toggleButton.style.border = 'none';
+    toggleButton.style.borderRadius = '5px';
+    toggleButton.style.boxShadow = '0 2px 5px rgba(0,0,0,0.25)';
+    toggleButton.textContent = isScriptEnabled ? '停止无限GPT-4插件' : '启动无限GPT-4插件';
+ 
+    toggleButton.onclick = toggleScript;
+ 
+    document.body.appendChild(toggleButton);
+ 
     let realFetch = window.fetch;
     window.fetch = function(url, init) {
-        if (init && init.method === 'POST') {
-            let data = JSON.parse(init.body);
-            if (data.hasOwnProperty('model')) {
-                data.model = 'gpt-4-mobile';
-                init.body = JSON.stringify(data);
-            }
+        if (!isScriptEnabled) {
+            return realFetch(url, init);
         }
-        return realFetch(url, init);
+        try {
+            if (init && init.method === 'POST') {
+                let data = JSON.parse(init.body);
+                if (data.hasOwnProperty('model')) {
+                    data.model = 'gpt-4-mobile';
+                    init.body = JSON.stringify(data);
+                }
+            }
+            return realFetch(url, init);
+        } catch (e) {
+            console.error('在处理请求时出现错误:', e);
+            return realFetch(url, init);
+        }
     };
 })();
